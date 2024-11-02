327a8a39a2013b30467a78acbe411873d40da0bf
Ash <redcoolike@gmail.com>
2024-11-02 13:51:33 +0100

    jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     env: {
       browser: true,
       es2021: true,
-      node: true,
+      jest: true,
     },
     extends: 'eslint:recommended',
     parserOptions: {
@@ -14,21 +14,7 @@ module.exports = {
       'no-unused-vars': ['error', { args: 'none' }],
     },
   };
-  module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-      'no-console': 'warn',
-      'no-unused-vars': ['error', { args: 'none' }],
-    },
-  };
+ 
+
 
   
